50b6966f5481c1e77a9c840ffd65e3f93200b823
IsmaelVargasG <is.vargar@alum.uca.es>
2024-03-19 12:54:41 +0100

    Stash

--- Assets/Scripts/Enemigos.cs
+++ Assets/Scripts/Enemigos.cs
@@ -6,21 +6,16 @@ using UnityEngine.AI;
 
 public class Enemigos : MonoBehaviour
 {
-<<<<<<< Updated upstream
     public float movementInterval = 0.16f;
     private NavMeshAgent pathfinder;
     private Transform target;
     private float movementTimer;
-=======
-    NavMeshAgent pathfinder;
-    Transform target;
 
     float vidaRestante;
     public Image imgBarraVida;
     public Canvas barraVida;
     public Camera playerCam;
 
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -33,14 +28,12 @@ public class Enemigos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         movementTimer -= Time.deltaTime;
         if(movementTimer < 0){
             pathfinder.SetDestination(target.position);
             movementTimer = movementInterval;
         }
 
-=======
         pathfinder.SetDestination(target.position);
         barraVida.transform.LookAt(playerCam.transform.position, Vector3.up);
     }
@@ -52,6 +45,5 @@ public class Enemigos : MonoBehaviour
 
     public void muelto(){
         Destroy(gameObject);
->>>>>>> Stashed changes
     }
 }
